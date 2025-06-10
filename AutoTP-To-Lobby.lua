49d99022be69abbc0542c0b57eefaec691e3a1b5
VernonMoore54 <denasuper1999@mail.ru>
2025-06-10 12:59:13 +0900

    Create AutoTP-To-Lobby.lua

---
+++ AutoTP-To-Lobby.lua
@@ -0,0 +1,31 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer.Character
+
+task.wait()
+
+setfpscap(15)
+
+
+task.wait(360)
+
+while true do
+    
+task.wait(2)
+-- Получаем необходимые сервисы
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+-- Сохраняем ссылку на локального игрока
+local player = Players.LocalPlayer
+
+-- Создаём и запускаем корутину
+local teleportCoroutine = coroutine.create(function()
+    -- Ждём 6 минуты (360 секунд)
+    -- Телепортируем игрока в placeId 116495829188952
+    TeleportService:Teleport(116495829188952, player)
+end)
+
+coroutine.resume(teleportCoroutine)
+
+end
