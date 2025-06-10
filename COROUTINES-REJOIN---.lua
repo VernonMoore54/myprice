a2a1224af9e5d6e042dbaad08013dd40325e69a5
VernonMoore54 <denasuper1999@mail.ru>
2025-06-10 21:35:56 +0900

    Update COROUTINES-REJOIN---.lua

--- COROUTINES-REJOIN---.lua
+++ COROUTINES-REJOIN---.lua
@@ -1,18 +1,1 @@
-local TS = game:GetService("TeleportService")
-local P = game:GetService("Players")
-local player = P.LocalPlayer
 
-local function waitForLoadingAndCheck()
-    local loadingGui = player:WaitForChild("PlayerGui"):WaitForChild("LoadingScreenPrefab", 20) 
-    if loadingGui then
-        task.wait(1) 
-        if loadingGui.Enabled then
-            while loadingGui.Enabled do
-                TS:Teleport(116495829188952, player)
-                task.wait(1)
-            end
-        end
-    end
-end
-
-coroutine.wrap(waitForLoadingAndCheck)()
