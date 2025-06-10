242e711102e5e2ae18099aafd1a3e052dda97a03
VernonMoore54 <denasuper1999@mail.ru>
2025-06-10 22:08:37 +0900

    Update COROUTINES-REJOIN---.lua

--- COROUTINES-REJOIN---.lua
+++ COROUTINES-REJOIN---.lua
@@ -1,27 +1,20 @@
-
--- Скрипт для автоматического телепорта после загрузочного экрана
-
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 local localPlayer = Players.LocalPlayer
-
--- Пытаемся дождаться появления ScreenGui LoadingScreenPrefab (таймаут 20 секунд)
 local playerGui = localPlayer:WaitForChild("PlayerGui")
-local loadingGui = playerGui:WaitForChild("LoadingScreenPrefab", 10)
 
--- Если объект не найден, выходим из скрипта
-if not loadingGui then
-    return
+task.wait(25)
+
+if game:GetService("Players").LocalPlayer.PlayerGui.LoadingScreenPrefab then
+
+while true do
+TeleportService:Teleport(116495829188952, localPlayer)
+task.wait(1)
 end
 
--- Дождаться 20 секунд после появления GUI
-task.wait(20)
+else
 
--- Проверяем, всё ещё ли GUI активен
-if loadingGui.Enabled then
-    -- Запускаем бесконечный цикл с задержкой 1 секунда
-    while true do
-        TeleportService:Teleport(116495829188952, localPlayer)
-        task.wait(1)
-    end
+while true do
+task.wait(3)
+end
 end
