95a6354797acf9ddf6d9be776a0103ceabe61130
VernonMoore54 <denasuper1999@mail.ru>
2025-06-17 19:53:11 +0900

    Update ALL-in-one-ded.lua

--- ALL-in-one-ded.lua
+++ ALL-in-one-ded.lua
@@ -14,9 +14,9 @@ local character = player.Character
 
 -- Нажимаем Space 10 раз с задержкой 0.5 секунды между нажатиями
 for i = 1, 10 do
-    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Space, false)
+    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Space, false, nil)
     task.wait(0.01) -- Короткая задержка для эмуляции нажатия
-    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Space, false)
+    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Space, false, nil)
     task.wait(0.5) -- Задержка между нажатиями
 end
 
@@ -30,6 +30,6 @@ end
 player.CharacterAdded:Wait()
 
 -- Зажимаем W на 2 секунды
-VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.W, false)
+VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.W, false, nil)
 task.wait(2)
-VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.W, false)
+VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.W, false, nil)
