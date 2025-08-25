d9d6a57339965c89a600bc99f7c654563f882511
VernonMoore54 <denasuper1999@mail.ru>
2025-08-25 16:25:30 +0900

    Update and rename queue_hop-4.lua to queue_hop-0.lua

--- queue_hop-0.lua
+++ queue_hop-0.lua
@@ -2,11 +2,16 @@ repeat
     task.wait(1)
 until game:IsLoaded()
 
-
-    local ZOV = loadstring(game:HttpGet('https://raw.githubusercontent.com/VernonMoore54/myprice/refs/heads/main/queue_hop-3.lua'))()
-
-    queue_on_teleport(ZOV)
-
+local teleportFunc = queueonteleport or queue_on_teleport
+if teleportFunc then
+    teleportFunc([[
+        if not game:IsLoaded() then
+            game.Loaded:Wait()
+        end
+        
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/VernonMoore54/myprice/refs/heads/main/queue_hop-0.lua"))()
+    ]])
+end
 
 local Players = game:GetService('Players')
 local TeleportService = game:GetService('TeleportService')
@@ -24,3 +29,4 @@ while true do
     end
     task.wait(3)
 end
+
