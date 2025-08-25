9becd2164af99aed5138cfc58b2f2ee91599d864
VernonMoore54 <denasuper1999@mail.ru>
2025-08-25 15:51:31 +0900

    Create queue_hop.lua

---
+++ queue_hop.lua
@@ -0,0 +1,39 @@
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer
+local queueteleport = (syn and syn.queue_on_teleport) 
+                     or queue_on_teleport 
+                     or (fluxus and fluxus.queue_on_teleport)
+
+-- Функция для самовоспроизводства
+local function setupQueue(scriptUrl)
+    if queueteleport then
+        queueteleport(string.format("loadstring(game:HttpGet('%s'))()", scriptUrl))
+    end
+end
+
+-- URL твоего скрипта
+local SCRIPT_URL = "https://raw.githubusercontent.com/yourusername/yourrepo/main/serverhop.lua"
+
+-- Настройка очереди при телепорте
+local TeleportCheck = false
+player.OnTeleport:Connect(function(State)
+    if not TeleportCheck and queueteleport then
+        TeleportCheck = true
+        setupQueue(SCRIPT_URL)
+    end
+end)
+
+-- Ждём персонажа
+local char = player.Character or player.CharacterAdded:Wait()
+
+-- Главный цикл хопа
+while true do
+    if game.PlaceId == 12807820316 then
+        TeleportService:Teleport(2809202155, player)
+    elseif game.PlaceId == 2809202155 then
+        TeleportService:Teleport(12807820316, player)
+    end
+    task.wait(3)
+end
