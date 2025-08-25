4ec6490f972158bd1693a84d14cff716ccbf3a0c
VernonMoore54 <denasuper1999@mail.ru>
2025-08-25 16:09:36 +0900

    Update and rename queue_hop-1.lua to queue_hop-0.lua

---
+++ queue_hop-0.lua
@@ -0,0 +1,25 @@
+repeat task.wait(1)
+until game:IsLoaded()
+
+queueteleport(loadstring(game:HttpGet("https://raw.githubusercontent.com/VernonMoore54/myprice/refs/heads/main/queue_hop-0.lua"))())
+
+do
+
+local Players = game:GetService('Players')
+local TeleportService = game:GetService('TeleportService')
+
+local player = Players.LocalPlayer
+
+-- Ждём персонажа
+local char = player.Character or player.CharacterAdded:Wait()
+
+while true do
+    if game.PlaceId == 12807820316 then
+        TeleportService:Teleport(2809202155, player)
+    elseif game.PlaceId == 2809202155 then
+        TeleportService:Teleport(12807820316, player)
+    end
+    task.wait(3)
+end
+
+end
