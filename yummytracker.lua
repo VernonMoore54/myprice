99cbb29d38576e0df22fbe9368f87949dbdf9bc2
VernonMoore54 <denasuper1999@mail.ru>
2026-02-27 01:29:03 +0900

    Create yummytracker.lua

---
+++ yummytracker.lua
@@ -0,0 +1,2 @@
+_G.Config = { UserID = "dff5c51a-d737-4d79-ae68-eb58fd23cd37", discord_id = "503941787195539456", Note = "farm" }
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/yummytrack/main/tracker", true))()
